bb482dc301c97634ec3ddd61e5b448b6c0076c52
arhanMucha <arhanmuchakani@gmail.com>
2026-04-05 13:41:57 +0200

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,8 +1,1 @@
-module.exports = async function handler(req, res) {
-  res.status(200).json({
-    businessName: process.env.BUSINESS_NAME || "Trattoria Roma",
-    businessHours: process.env.BUSINESS_HOURS || "Tue-Sun 12:00-22:00",
-    businessAddress: process.env.BUSINESS_ADDRESS || "Maximilianstraße 8, Munich",
-    businessPhone: process.env.BUSINESS_PHONE || "+49 89 456789"
-  });
-};
+
