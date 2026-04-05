44794b515158940721ae1c1b70acec188f8e7e86
arhanMucha <arhanmuchakani@gmail.com>
2026-04-05 13:47:12 +0200

    Add calendar status API endpoint

---
+++ api/calendar-status.js
@@ -0,0 +1,10 @@
+module.exports = async function handler(req, res) {
+  const connected = Boolean(
+    process.env.GOOGLE_CLIENT_ID &&
+    process.env.GOOGLE_CLIENT_SECRET &&
+    process.env.GOOGLE_REDIRECT_URI &&
+    process.env.GOOGLE_CALENDAR_ID
+  );
+
+  res.status(200).json({ connected });
+};
